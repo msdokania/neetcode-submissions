6c622590bf62e6f7c23b65c6f178ae412dab887b
Monalisa Dokania <95061947+msdokania@users.noreply.github.com>
2026-06-07 17:41:24 -0700

    Add: palindromic-substrings - submission-3

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-3.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int countSubstrings(String s) {
+        int ans = 0;
+        int n = s.length();
+        for(int i=0; i<n; i++) {
+            int l=i, r=i;
+            while(l>=0 && r<n && s.charAt(l)==s.charAt(r)) {
+                ans++;
+                l--;
+                r++;
+            }
+        }
+        for(int i=0; i<n; i++) {
+            int l=i, r=i+1;
+            while(l>=0 && r<n && s.charAt(l)==s.charAt(r)) {
+                ans++;
+                l--;
+                r++;
+            }
+        }
+        return ans;
+    }
+}
