954d6165f3f65bcbc2c9d74c4416339043b35166
Monalisa Dokania <95061947+msdokania@users.noreply.github.com>
2026-06-07 21:06:07 -0700

    Add: best-time-to-buy-and-sell-stock-ii - submission-2

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int ans = 0;
+        int buy = prices[0];
+        for(int i=1; i<prices.length; i++) {
+            if(prices[i]<buy) {
+                buy = prices[i];
+            } else {
+                ans += prices[i]-buy;
+                buy = prices[i];
+            }
+
+        }
+        return ans;
+    }
+}
