c43e306a41322b9b0867e8f8828982b2985d322c
Monalisa Dokania <95061947+msdokania@users.noreply.github.com>
2026-07-08 05:59:52 -0700

    Add: house-robber-iii - submission-5

---
+++ Data Structures & Algorithms/house-robber-iii/submission-5.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rob(self, root: Optional[TreeNode]) -> int:
+        def helper(node: Optional[TreeNode]) -> List[int]:
+            if not node:
+                return [0,0]
+            a = helper(node.left)
+            b = helper(node.right)
+            r = node.val + a[1]+b[1]
+            nr = max(a[0],a[1]) + max(b[0],b[1])
+            return [r,nr]
+            
+        ans = helper(root)
+        return max(ans[0],ans[1])
