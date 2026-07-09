aec88ba041612b0b4da2117987aff7bbb3ea0fa2
Monalisa Dokania <95061947+msdokania@users.noreply.github.com>
2026-07-08 18:04:47 -0700

    Add: matchsticks-to-square - submission-4

---
+++ Data Structures & Algorithms/matchsticks-to-square/submission-4.py
@@ -0,0 +1,23 @@
+class Solution:
+    def makesquare(self, matchsticks: List[int]) -> bool:
+        side = sum(matchsticks)
+        if side%4 != 0:
+            return False
+        side = side//4
+
+        matchsticks.sort(reverse=True)
+        if matchsticks[0]>side:
+            return False
+
+        def helper(i: int, arr: List[int]):
+            if i==len(matchsticks):
+                return True
+            ret = False
+            for j in range(4):
+                if arr[j]+matchsticks[i] <= side:
+                    arr[j] += matchsticks[i]
+                    ret = ret or helper(i+1, arr)
+                    arr[j] -= matchsticks[i]
+            return ret
+
+        return helper(0, [0,0,0,0])
