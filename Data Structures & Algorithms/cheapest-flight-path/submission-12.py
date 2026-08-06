56c0f7231b015fecb2a5b5922a8dd12f6e6941cb
Monalisa Dokania <95061947+msdokania@users.noreply.github.com>
2026-08-06 10:28:01 -0700

    Add: cheapest-flight-path - submission-12

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-12.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        cost = [float("inf")]*n
+        cost[src] = 0
+        for i in range(k+1):
+            tmp = cost.copy()
+            for f in flights:
+                newcost = cost[f[0]]+f[2]
+                tmp[f[1]] = min(tmp[f[1]], newcost)
+            cost = tmp
+        return cost[dst] if cost[dst]!=float("inf") else -1
+        
