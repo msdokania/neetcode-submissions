eaf2de3326aa10405a657045d1760d9f796705d7
Monalisa Dokania <95061947+msdokania@users.noreply.github.com>
2026-08-12 11:08:32 -0700

    Add: network-delay-time - submission-8

---
+++ Data Structures & Algorithms/network-delay-time/submission-8.py
@@ -0,0 +1,22 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        timearr = [float("inf")]*n
+        timearr[k-1] = 0
+        adj = defaultdict(list)
+        for u,v,t in times:
+            adj[u].append((v,t))
+        # q = deque()
+        q = [(0, k)]
+        # q.append((0,k))
+        while q:
+            currtime,u = heapq.heappop(q)
+            if timearr[u-1]<currtime:
+                continue
+            for v,t in adj[u]:
+                newtime = t+currtime
+                if newtime<timearr[v-1]:
+                    timearr[v-1] = newtime
+                    # q.append((newtime,v))
+                    heapq.heappush(q, (newtime,v))
+        ans = max(timearr)
+        return ans if ans!=float("inf") else -1
