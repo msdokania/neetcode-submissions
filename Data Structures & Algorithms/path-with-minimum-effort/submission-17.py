f5afbf22e76d159935d870f9b3d013f627c8db86
Monalisa Dokania <95061947+msdokania@users.noreply.github.com>
2026-08-12 10:57:43 -0700

    Add: path-with-minimum-effort - submission-17

---
+++ Data Structures & Algorithms/path-with-minimum-effort/submission-17.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        m,n = len(heights), len(heights[0])
+        dist = [[float("inf")]*n for _ in range(m)]
+        dist[0][0] = 0
+        q = deque()
+        q.append((0,0,0))
+        # heapq.heappush(q, (0,0,0))
+        while q:
+            curr,r,c = q.popleft()
+            if dist[r][c]<curr:
+                continue
+            # if r==m-1 and c==n-1:
+            #     return curr
+            for nr,nc in [(r+1,c),(r-1,c),(r,c+1),(r,c-1)]:
+                if not 0<=nr<m or not 0<=nc<n:
+                    continue
+                neweffort = max(curr, abs(heights[nr][nc]-heights[r][c]))
+                if dist[nr][nc]>neweffort:
+                    dist[nr][nc] = neweffort
+                    q.append((neweffort,nr,nc))
+                    # heapq.heappush(q, (neweffort,nr,nc))
+        return dist[m-1][n-1] if dist[m-1][n-1]!=float("inf") else -1
